3218d8b50909c2e2f6e8aa420a7c0d4407f531a4
danchurch <danchurchthomas@gmail.com>
2017-05-22 18:22:40 -0700

    cleaning up

---
+++ sweeps/scripts/newwood_d5.py
@@ -0,0 +1,19 @@
+######## sweeping wood deposition rates ################
+
+decomp_runs = {}
+for i in [ i for i in range(1,13,1) ]: ## levels of variable
+    #endo_list = []
+    decomp_list = []
+    for j in range(100): ## number of simulations per level of parameter
+        losced = Forest(endophytism = False, ##settings for model
+                        newwood=i, ## parameter of interest
+                        decompdisp=5, ## hold fungal dispersal here
+                        endodisp=0, ## no blue fungi
+                        )
+        for j in range(50): losced.step() ## number of steps before ending the model
+        ## data into lists
+        #endo_list.append(losced.datacollector.get_model_vars_dataframe().Endophytes)
+        decomp_list.append(losced.datacollector.get_model_vars_dataframe().Decomposers)
+    decomp_runs[i] = decomp_list
+
+pickle.dump(decomp_runs, open('newwood_d5.p','wb'))
---
+++ sweeps/scripts/ws_d5.py
@@ -0,0 +1,21 @@
+############# sweep initial wood ###############
+
+
+decomp_runs = {}
+for i in [ i for i in range(20,80,10) ]: ## levels of variable
+    #endo_list = []
+    decomp_list = []
+    for j in range(100): ## number of simulations per level of parameter
+        losced = Forest(endophytism = False, ##settings for model
+                        ws=i, ## parameter of interest
+                        decompdisp=5, ## hold fungal dispersal here
+                        endodisp=0, ## no blue fungi
+                        )
+        for j in range(50): losced.step() ## number of steps before ending the model
+        ## data into lists
+        #endo_list.append(losced.datacollector.get_model_vars_dataframe().Endophytes)
+        decomp_list.append(losced.datacollector.get_model_vars_dataframe().Decomposers)
+    decomp_runs[i] = decomp_list
+
+pickle.dump(decomp_runs, open('ws_d6.p','wb'))
+
