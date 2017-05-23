3025bf745343751bc3690922960ffd451b32984f
danchurch <danchurchthomas@gmail.com>
2017-05-22 18:14:44 -0700

    reorganize sweeps

--- FAserver.py
+++ FAserver.py
@@ -49,4 +49,4 @@ def agent_portrayal(agent):
 canvas_element = CanvasGrid(agent_portrayal, 100,100,500,500)
 
 
-server = ModularServer(Forest, [canvas_element], "TreesFungiWood", ws=30, decompdisp=6, endodisp=0, woodfreq = 2, newwood = 8)
+server = ModularServer(Forest, [canvas_element], "TreesFungiWood", ws=70, decompdisp=5, endodisp=0)
