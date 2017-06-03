28679e51713c9b2990b008820389b66bb6e13f53
danchurch <danchurchthomas@gmail.com>
2017-06-03 14:41:03 -0700

    please cluster work

--- onefungus_disp.py
+++ onefungus_disp.py
@@ -17,32 +17,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import thomasprocess as tp
 
+with open('updates.txt', 'a') as upd:
+    lvls = [ a for a in range(1,13,1) ]
+    runs = {}
+    for i in lvls: ## levels of variable
+        run_list = []
+        upd.write('starting level: %s \n' %i)
+        for j in range(100): ## number of simulations per level of parameter
+            losced = Forest(
+                    endophytism = False, ## Turn off endos
+                    ws = 30,
+                    endodisp=0, ## Turn off endos
+                    decompdisp=i, ## variable of interest
+                    leafdisp = 4,
+                    leaffall = 1,
+                    numdecomp=1,
+                    numendo=1,
+                    endoloss=0.05,
+                    newwood = 6,
+                    woodfreq = 1,
+                    width = 100,
+                    kappa = 0.03,
+                    sigma = 3.0,
+                    mu = 2.2,
+                            )
+            for j in range(50): losced.step() ## number of steps before ending the model
+            run_list.append(losced.datacollector.get_model_vars_dataframe())
+        upd.write('completed level: %s \n' %i)
+            ## data into lists
+        runs[i] = run_list
 
-lvls = [ a for a in range(1,13,1) ]
-runs = {}
-for i in lvls: ## levels of variable
-    run_list = []
-    for j in range(100): ## number of simulations per level of parameter
-        losced = Forest(
-                endophytism = False, ## Turn off endos
-                ws = 30,
-                endodisp=0, ## Turn off endos
-                decompdisp=i, ## variable of interest
-                leafdisp = 4,
-                leaffall = 1,
-                numdecomp=1,
-                numendo=1,
-                endoloss=0.05,
-                newwood = 6,
-                woodfreq = 1,
-                width = 100,
-                kappa = 0.03,
-                sigma = 3.0,
-                mu = 2.2,
-                        )
-        for j in range(50): losced.step() ## number of steps before ending the model
-        ## data into lists
-        run_list.append(losced.datacollector.get_model_vars_dataframe())
-    runs[i] = run_list
-    
-pickle.dump(runs, open('/sweeps/results/onefungus_disp.p', 'wb')) 
+pickle.dump(runs, open('onefungus_disp.p', 'wb'))
+
