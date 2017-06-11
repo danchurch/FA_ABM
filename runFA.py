46fe6c8650d1aaa4bb46dacfc8c956417a0c2b89
danchurch <danchurchthomas@gmail.com>
2017-06-11 15:45:49 -0700

    too many trees?

--- runFA.py
+++ runFA.py
@@ -11,14 +11,13 @@ from FAagents import Tree, Wood, Fungus
 import numpy as np
 import matplotlib.pyplot as plt
 import thomasprocess as tp
-
 import argparse
 import datetime
+import logging
+
 
 ## instantiate our parser:
 parser = argparse.ArgumentParser()
-
-losced = Forest()
 
 ## model parameters:
 parser.add_argument("-no_endophytism", action='store_false', required=False)
@@ -36,16 +35,24 @@ parser.add_argument("-width", type=float, required=False)
 parser.add_argument("-kappa", type=float, required=False)
 parser.add_argument("-sigma", type=float, required=False)
 parser.add_argument("-mu", type=float, required=False)
-
 ## set number of simulations:
 parser.add_argument("-sims", type=int, required=True)
-
 ## output pickle file name:
 parser.add_argument("-fileout", required=False)
 
 
 ## get our commandline arguments into the environment
 args = parser.parse_args()
+
+## set our filename
+if args.fileout: ## if one was set by user
+    fileout = args.fileout
+else: ## if not, timestamp
+    fileout = 'run.' + str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).replace(':', '.') 
+
+## set log
+logging.basicConfig(filename=fileout + '.log',level=logging.INFO)
+
 
 run_list = [] 
 for j in range(args.sims): ## number of simulations per level of parameter
@@ -63,12 +70,14 @@ for j in range(args.sims): ## number of simulations per level of parameter
     if args.newwood: losced.newwood = args.newwood 
     if args.woodfreq: losced.woodfreq = args.woodfreq 
     if args.width: losced.width = args.width 
-    ## the thomas process settings also have to be put in place at the instantiation of the model:
+    ## the tree settings also have to be put in place at the instantiation of the model:
     if args.kappa: losced = Forest(endophytism = args.no_endophytism, kappa = args.kappa)
     if args.sigma: losced = Forest(endophytism = args.no_endophytism, sigma = args.sigma)
     if args.mu: losced = Forest(endophytism = args.no_endophytism, mu = args.mu)
-    #for k in range(50): losced.step() ## number of steps before ending the model
-    for k in range(2): losced.step() ## testing_________
+    #for k in range(50):  ## number of steps before ending the model
+    for k in range(2): ## test, just two steps 
+        losced.step() 
+        logging.info('run %s step %s' %(j,k))
     run_list.append(losced.datacollector.get_model_vars_dataframe())
     ## data into lists
 
@@ -77,14 +86,9 @@ for j in range(args.sims): ## number of simulations per level of parameter
 #print('args.endodisp? = %s' %args.endodisp)
 #print('losced.endodisp? = %s' %losced.endodisp)
 
-if args.fileout:
-    fileout = args.fileout
-else:
-    fileout = 'run.' + str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).replace(':', '.') + '.p'
 
-pickle.dump(run_list, open('%s' %fileout, 'wb'))
+picklefile = fileout + ".p"
+pickle.dump(run_list, open(picklefile, 'wb'))
 
 
 
-
-uniq_filename = str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).replace(':', '.')
